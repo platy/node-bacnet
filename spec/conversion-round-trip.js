2fd4725f59cd25ab0fc70acb3c5c9174af4bf55c
Mike Bush <mpbush@gmail.com>
2016-02-16 17:01:02 +0100

    Style fixes

--- spec/conversion-round-trip.js
+++ spec/conversion-round-trip.js
@@ -1,5 +1,5 @@
 /* globals it, describe */
-"use strict"
+'use strict'
 
 const should = require('should')
 const BacnetValue = require('../bacnet.js').BacnetValue
@@ -10,7 +10,7 @@ describe('Application value converters between BACnet and js domain', () => {
     BacnetValue(0, 4).should.not.deepEqual(BacnetValue(0, 5))
     BacnetValue(0, 4).should.not.deepEqual(BacnetValue(1, 4))
   })
-  function roundTripConversionTest(inputs, roundTripTest) {
+  function roundTripConversionTest (inputs, roundTripTest) {
     inputs.forEach((input) => {
       it('works for [' + input + ']', () => {
         roundTripTest(input)
@@ -21,7 +21,7 @@ describe('Application value converters between BACnet and js domain', () => {
     roundTripConversionTest([null], (value) => {
       let bacnetValue = new BacnetValue(value, 0)
       should(bacnetValue.valueOf()).equal(value)
-      let bacnetBytes = bacnetValue.bytes();
+      let bacnetBytes = bacnetValue.bytes()
       BacnetValue.fromBytes(bacnetBytes).should.deepEqual(bacnetValue)
     })
   })
@@ -29,7 +29,7 @@ describe('Application value converters between BACnet and js domain', () => {
     roundTripConversionTest([true, false], (value) => {
       let bacnetValue = new BacnetValue(value, 1)
       bacnetValue.valueOf().should.equal(value)
-      let bacnetBytes = bacnetValue.bytes();
+      let bacnetBytes = bacnetValue.bytes()
       BacnetValue.fromBytes(bacnetBytes).should.deepEqual(bacnetValue)
     })
   })
@@ -37,7 +37,7 @@ describe('Application value converters between BACnet and js domain', () => {
     roundTripConversionTest([0, 4000000000], (value) => {
       let bacnetValue = new BacnetValue(value, 2)
       bacnetValue.valueOf().should.equal(value)
-      let bacnetBytes = bacnetValue.bytes();
+      let bacnetBytes = bacnetValue.bytes()
       BacnetValue.fromBytes(bacnetBytes).should.deepEqual(bacnetValue)
     })
   })
@@ -45,15 +45,15 @@ describe('Application value converters between BACnet and js domain', () => {
     roundTripConversionTest([-2000000000, 0, 2000000000], (value) => {
       let bacnetValue = new BacnetValue(value, 3)
       bacnetValue.valueOf().should.equal(value)
-      let bacnetBytes = bacnetValue.bytes();
+      let bacnetBytes = bacnetValue.bytes()
       BacnetValue.fromBytes(bacnetBytes).should.deepEqual(bacnetValue)
     })
   })
-  describe('float conversion roundtrip', () => {    /// Float loses some precision during change between double (in js) and float
-    roundTripConversionTest([0, 2^-30, -2^-30, 1e10, -1e10], (value) => {
+  describe('float conversion roundtrip', () => {    // Float loses some precision during change between double (in js) and float
+    roundTripConversionTest([0, 2 ^ -30, -2 ^ -30, 1e10, -1e10], (value) => {
       let bacnetValue = new BacnetValue(value, 4)
       Number(bacnetValue).should.equal(value)
-      let bacnetBytes = bacnetValue.bytes();
+      let bacnetBytes = bacnetValue.bytes()
       BacnetValue.fromBytes(bacnetBytes).should.deepEqual(bacnetValue)
     })
   })
@@ -61,7 +61,7 @@ describe('Application value converters between BACnet and js domain', () => {
     roundTripConversionTest([0, 1e-37, -1e-37, 1e10, -1e10, Number.MAX_VALUE, Number.MIN_VALUE], (value) => {
       let bacnetValue = new BacnetValue(value, 5)
       Number(bacnetValue).should.equal(value)
-      let bacnetBytes = bacnetValue.bytes();
+      let bacnetBytes = bacnetValue.bytes()
       BacnetValue.fromBytes(bacnetBytes).should.deepEqual(bacnetValue)
     })
   })
@@ -69,15 +69,15 @@ describe('Application value converters between BACnet and js domain', () => {
     roundTripConversionTest([new Buffer(0), new Buffer([0, 1, 2])], (value) => {
       let bacnetValue = new BacnetValue(value, 6)
       bacnetValue.valueOf().should.equal(value)
-      let bacnetBytes = bacnetValue.bytes();
+      let bacnetBytes = bacnetValue.bytes()
       BacnetValue.fromBytes(bacnetBytes).should.deepEqual(bacnetValue)
     })
   })
   describe('character string conversion roundtrip', () => {
-    roundTripConversionTest(["hello", 'world'], (value) => {
+    roundTripConversionTest(['hello', 'world'], (value) => {
       let bacnetValue = new BacnetValue(value, 7)
       bacnetValue.valueOf().should.equal(value)
-      let bacnetBytes = bacnetValue.bytes();
+      let bacnetBytes = bacnetValue.bytes()
       BacnetValue.fromBytes(bacnetBytes).should.deepEqual(bacnetValue)
     })
   })
