3eb9e1983990c9a27aa2458ace2efc3fb6375526
Mike Bush <mpbush@gmail.com>
2016-03-04 11:12:22 +0100

    Add a write property example

---
+++ examples/writeProperty.js
@@ -0,0 +1,26 @@
+const bacnet = require('../bacnet.js')
+const r = bacnet.init({
+  datalink: {
+    iface: 'bridge100',
+    ip_port: 0xBAC0
+  },
+  device: false
+})
+
+console.log('reading property', bacnet.propertyKeyToString(process.argv[5]))
+
+function objectIdToString (objectId) {
+  return bacnet.objectTypeToString(objectId.type) + '/' + objectId.instance
+}
+
+r.on('iam', function (iam) {
+  console.log('iam: ', iam)
+  r.writeProperty(Number(process.argv[2]), process.argv[3], process.argv[4], process.argv[5], false, JSON.parse(process.argv[6]), function (err) {
+    if (err) console.log('error', err)
+    else console.log('success')
+  })
+})
+
+r.whois(Number(process.argv[2]))
+
+setTimeout(function () {}, 1000)
