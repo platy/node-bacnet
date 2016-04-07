bdc61a592318cb41e6601859de274b2b23b1669d
Mike Bush <mpbush@gmail.com>
2016-04-07 16:30:22 +0200

    Open a port instead of trying to initialise 2 - it looks like maybe the linux port reuses ports but the osx one does not

--- spec/initialisationErrors.js
+++ spec/initialisationErrors.js
@@ -1,5 +1,5 @@
 'use strict'
-/* global describe, xit, before, after */
+/* global describe, it, before, after */
 
 require('should')
 const tools = require('./tools')
@@ -7,42 +7,33 @@ const tools = require('./tools')
 const iface = tools.getSuitableBroadcastInterface()
 
 describe('Initialisation', function () {
-  var device, device2
-  after('Exit the device1 fork', function (done) {
-    device.once('exit', done)
-    device.exit()
-  })
+  var device
   after('Exit the device2 fork', function (done) {
-    if (device2) {
-      device2.removeAllListeners()
-      device2.once('exit', done)
-      device2.exit()
+    if (device) {
+      device.removeAllListeners()
+      device.once('exit', done)
+      device.exit()
     } else {
       done()
     }
   })
   before(function (done) {
+    var socket = require('dgram').createSocket('udp4').bind(47808)
+    socket.once('listening', done)
+  })
+  // disabled as currently the error doesn't work on linux - annoying
+  it('fails if the port is already in use', (done) => {
     device = tools.deviceProcess({
       datalink: {
         iface: iface
       },
       device: true
     })
-    device.once('up', done)
-  })
-  // disabled as currently the error doesn't work on linux - annoying
-  xit('fails if the port is already in use', (done) => {
-    device2 = tools.deviceProcess({
-      datalink: {
-        iface: iface
-      },
-      device: true
-    })
-    device2.once('up', () => done(new Error('Second device reported as up on port which is already in use')))
-    device2.once('init-error', (err) => {
+    device.once('up', () => done(new Error('Second device reported as up on port which is already in use')))
+    device.once('init-error', (err) => {
       err.should.equal('Error: Failed to initialize data link')
       done()
     })
-    device2.once('exit', (exitCode) => done(new Error('Bacnet client terminated prematurely with exit code : ' + exitCode)))
+    device.once('exit', (exitCode) => done(new Error('Bacnet client terminated prematurely with exit code : ' + exitCode)))
   })
 })
