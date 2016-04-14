6d369c39bc0f546afdf8b9d854997e768b80ef89
Mike Bush <mpbush@gmail.com>
2016-04-14 11:27:03 +0200

    Add isBound into module

--- src/module.cc
+++ src/module.cc
@@ -46,6 +46,8 @@ NAN_METHOD(InitInstance) {
     Local<Object> target = New<Object>();
     Nan::Set(target, New("whois").ToLocalChecked(),
       Nan::GetFunction(New<FunctionTemplate>(whois)).ToLocalChecked());
+    Nan::Set(target, New("isBound").ToLocalChecked(),
+      Nan::GetFunction(New<FunctionTemplate>(isBound)).ToLocalChecked());
     Nan::Set(target, New("readProperty").ToLocalChecked(),
       Nan::GetFunction(New<FunctionTemplate>(readProperty)).ToLocalChecked());
     Nan::Set(target, New("writeProperty").ToLocalChecked(),
