24b71c66254367d2419c976f5e72490c0384fa43
Mike Bush <mpbush@gmail.com>
2016-04-22 18:27:05 +0200

    Extract some functions in bacnet.js

--- bacnet.js
+++ bacnet.js
@@ -1,15 +1,21 @@
 const addon = require('./build/Release/binding.node')
 const bacnet = Object.create(addon)
+const EventEmitter = require('events').EventEmitter
+const util = require('util')
 
-bacnet.init = function init (config) {
-  const EventEmitter = require('events').EventEmitter
-  var flatConfig = config && config.datalink || {} // I've flattened the config as I had trouble getting nested properties in the c++
-  flatConfig.device_instance_id = config.device_instance_id
-  const bacnetAddon = addon.init(flatConfig)
-
-  const bacnetInterface = new EventEmitter()
+function BACnetInstance(config) {
+  EventEmitter.call(this)
+  const bacnetAddon = addon.init(flattenConfig(config))
+  bacnetAddon.initClient(this)
+  if (config && config.device) bacnetAddon.initDevice()
+  bacnetAddon.listen()
 
   const confirmedCallbacks = {}
+  setupMethods.call(this, bacnetAddon, confirmedCallbacks)
+  setupHandlers.call(this, confirmedCallbacks)
+}
+
+function setupMethods (bacnetAddon, confirmedCallbacks) {
   function addCallback (invokeId, callback) {
     if (callback && invokeId > 0) {
       if (typeof callback !== 'function') throw new TypeError('non-function passed as callback argument')
@@ -17,21 +23,17 @@ bacnet.init = function init (config) {
     }
     return invokeId
   }
-
-  bacnetAddon.initClient(bacnetInterface)
-  if (config && config.device) bacnetAddon.initDevice()
-  bacnetAddon.listen()
-
-  bacnetInterface.closeQueue = bacnetAddon.closeQueue
-  bacnetInterface.whois = bacnetAddon.whois
-  bacnetInterface.isBound = bacnetAddon.isBound
-  bacnetInterface.readProperty = function (deviceInstance, objectType, objectInstance, property, arrayIndex, callback) {
+  
+  this.closeQueue = bacnetAddon.closeQueue
+  this.whois = bacnetAddon.whois
+  this.isBound = bacnetAddon.isBound
+  this.readProperty = function (deviceInstance, objectType, objectInstance, property, arrayIndex, callback) {
     if (!objectType) throw new TypeError('Expected an object type, got : ' + objectType)
     const invokeId = bacnetAddon.readProperty(deviceInstance, bacnet.objectTypeToNumber(objectType), objectInstance, bacnet.propertyKeyToNumber(property), arrayIndex)
     if (invokeId === 0) throw new Error('Invoking BACnet read failed')
     return addCallback(invokeId, callback)
   }
-  bacnetInterface.writeProperty = function (deviceInstance, objectType, objectInstance, property, arrayIndex, value, callback) {
+  this.writeProperty = function (deviceInstance, objectType, objectInstance, property, arrayIndex, value, callback) {
     if (!objectType) throw new TypeError('Expected an object type, got : ' + objectType)
     if (value.constructor !== bacnet.BacnetValue) {
       value = new bacnet.BacnetValue(value)
@@ -40,7 +42,9 @@ bacnet.init = function init (config) {
     if (invokeId === 0) throw new Error('Invoking BACnet read failed')
     return addCallback(invokeId, callback)
   }
+}
 
+function setupHandlers(confirmedCallbacks) {
   function executeCallback () {
     const invocationCallback = confirmedCallbacks[ this ]
     if (invocationCallback) {
@@ -49,31 +53,53 @@ bacnet.init = function init (config) {
         invocationCallback.apply(null, arguments)
       } catch (err) {
         console.log('Error in callback', err.stack)
-        bacnetInterface.emit('error', err)
+        this.emit('error', err)
       }
     }
   }
 
-  bacnetInterface.on('ack', function (invokeId, response) {
+  this.on('ack', function onAck (invokeId, response) {
     executeCallback.call(invokeId, null, response)
   })
 
-  bacnetInterface.on('abort', function (invokeId, reason) {
+  this.on('abort', function onAbort (invokeId, reason) {
     console.log('abort', invokeId)
     executeCallback(invokeId, new Error(reason))
   })
 
-  bacnetInterface.on('reject', function (invokeId, reason) {
+  this.on('reject', function onReject (invokeId, reason) {
     console.log('abort', invokeId)
     executeCallback(invokeId, new Error(reason))
   })
 
-  bacnetInterface.on('error-ack', function (invokeId, error) {
+  this.on('error-ack', function onErrorAck (invokeId, error) {
     console.log('error-ack', invokeId, error)
-    executeCallback(invokeId, new Error('Error received in acknowledgment for request #' + invokeId + ' ' + error['error-class'] + '/' + error['error-code']))
+    executeCallback(invokeId, new Error('Error received in acknowledgment for request #' + invokeId + ' ' + error[ 'error-class' ] + '/' + error[ 'error-code' ]))
   })
+}
 
-  return bacnetInterface
+function flattenConfig (config) {
+  var flatConfig = config && config.datalink || {} // I've flattened the config as I had trouble getting nested properties in the c++
+  flatConfig.device_instance_id = config.device_instance_id
+  return flatConfig
+}
+
+util.inherits(BACnetInstance, EventEmitter)
+
+/**
+ * {
+ *   datalink: {
+ *     iface: process.env.BACNET_INTERFACE,
+ *     ip_port: process.env.BACNET_PORT || 0xBAC0
+ *   },
+ *   device: false,
+ *   device_instance_id: 12
+ * }
+ * @param config
+ * @returns {*|EventEmitter}
+ */
+bacnet.init = function init (config) {
+  return new BACnetInstance(config)
 }
 
 module.exports = bacnet
